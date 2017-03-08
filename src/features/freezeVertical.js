fb3b5cbc40d0e09db37a80ff29ea9a60e78db078
chenshu <chenshu@ehousechina.com>
2017-03-08 18:46:14 +0800

    verify if touchevent is available

--- src/features/freezeVertical.js
+++ src/features/freezeVertical.js
@@ -30,61 +30,66 @@ let config = {
         }
     },
     created(){
-        this.$on("panoCreated", krpano => {
 
-            let origin = krpano.firstChild;
+        if (TouchEvent) {
 
-            let eventDelegate = document.createElement("DIV");
-            eventDelegate.className = "event-delegate";
-            Object.assign(eventDelegate.style, {
-                display: this.freezeVertical ? "block" : "none",
-                width: "100%",
-                height: "100%",
-                position: "absolute",
-                "user-select": "none",
-                "z-index": 1
+            this.$on("panoCreated", krpano => {
+
+                let origin = krpano.firstChild;
+
+                let eventDelegate = document.createElement("DIV");
+                eventDelegate.className = "event-delegate";
+                Object.assign(eventDelegate.style, {
+                    display: this.freezeVertical ? "block" : "none",
+                    width: "100%",
+                    height: "100%",
+                    position: "absolute",
+                    "user-select": "none",
+                    "z-index": 1
+                });
+                krpano.appendChild(eventDelegate);
+                this.eventDelegate = eventDelegate;
+
+                let originTouch;
+
+                eventDelegate.addEventListener("touchstart", function (e) {
+                    originTouch = e;
+                    Object.defineProperty(e, "cancelable", {value: true});
+                    let event = new TouchEvent("touchstart", e);
+                    return origin.dispatchEvent(event);
+                });
+
+                eventDelegate.addEventListener("touchend", function (e) {
+                    Object.defineProperty(e, "cancelable", {value: true});
+                    let event = new TouchEvent("touchend", e);
+                    return origin.dispatchEvent(event);
+                });
+
+                eventDelegate.addEventListener("touchmove", function (e) {
+
+                    let currentTouch = e.changedTouches[0];
+                    let deltaX = originTouch.clientX - currentTouch.clientX;
+                    let hlookat = krpano.get("view.hlookat") + deltaX;
+                    let vlookat = krpano.get("view.vlookat");
+                    krpano.call(`lookat(${hlookat},${vlookat})`);
+
+                    originTouch = currentTouch;
+
+                    return true;
+                });
+
+                eventDelegate.addEventListener("mousedown", function (e) {
+                    Object.defineProperty(e, "cancelable", {value: true});
+                    return origin.dispatchEvent(new MouseEvent("mousedown", e));
+                });
+
+                eventDelegate.addEventListener("mousewheel", function (e) {
+                    Object.defineProperty(e, "cancelable", {value: true});
+                    return origin.dispatchEvent(new MouseEvent("mousewheel", e));
+                });
             });
-            krpano.appendChild(eventDelegate);
-            this.eventDelegate = eventDelegate;
+        }
 
-            let originTouch;
-
-            eventDelegate.addEventListener("touchstart", function (e) {
-                originTouch = e;
-                Object.defineProperty(e, "cancelable", {value: true});
-                let event = new TouchEvent("touchstart", e);
-                return origin.dispatchEvent(event);
-            });
-
-            eventDelegate.addEventListener("touchend", function (e) {
-                Object.defineProperty(e, "cancelable", {value: true});
-                let event = new TouchEvent("touchend", e);
-                return origin.dispatchEvent(event);
-            });
-
-            eventDelegate.addEventListener("touchmove", function (e) {
-
-                let currentTouch = e.changedTouches[0];
-                let deltaX = originTouch.clientX - currentTouch.clientX;
-                let hlookat = krpano.get("view.hlookat") + deltaX;
-                let vlookat = krpano.get("view.vlookat");
-                krpano.call(`lookat(${hlookat},${vlookat})`);
-
-                originTouch = currentTouch;
-
-                return true;
-            });
-
-            eventDelegate.addEventListener("mousedown", function (e) {
-                Object.defineProperty(e, "cancelable", {value: true});
-                return origin.dispatchEvent(new MouseEvent("mousedown", e));
-            });
-
-            eventDelegate.addEventListener("mousewheel", function (e) {
-                Object.defineProperty(e, "cancelable", {value: true});
-                return origin.dispatchEvent(new MouseEvent("mousewheel", e));
-            });
-        });
     }
 };
 
